fd6f4423728d542474cba081f67594db06d5cdf3
Benjamin Jorand <benjamin.jorand@gmail.com>
2016-09-10 19:57:59 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+set -ex
+
 mkdir -p bin
 if [[ ! -f ./bin/hugo ]];
 then
 	pushd bin
 	wget https://github.com/spf13/hugo/releases/download/v0.16/hugo_0.16_linux-64bit.tgz
-	tar xvzf hugo_0.16_linux-64bit.tgz hugo
+	tar xvzf hugo_0.16_linux-64bit.tgz
 	rm -f hugo_0.16_linux-64bit.tgz
+	chmod +x hugo
 	popd
 fi
 ./bin/hugo
